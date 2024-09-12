9733ba326ff8e557ee04cc91a037325357d25b44
mozarelly <mapcwork@gmail.com>
2024-09-12 14:32:39 -0400

    Initial Commit
    Código desarrollado hasta la fecha

---
+++ sistema_uma/test_data_insertion.py
@@ -0,0 +1,8 @@
+# test_db.py
+# test the data insertion into database with dummy data
+from UMA_Db import UMA_Db
+
+UMA_Db = UMA_Db(hostStr="localhost", dbPort=5432,
+                    dbStr="uma", uNameStr="postgres", dbPassStr="123456")
+UMA_Db.insertData('Piso 3', 15, 12, 20, 121, 18, 18, 18, 19, False, 89, 17, 19, 71)
+print("data insertion testing complete...")
