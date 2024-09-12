7d227acc4709234f4c39e1acd099485ca5c4380d
mozarelly <mapcwork@gmail.com>
2024-09-12 14:34:22 -0400

    Avance hasta ahora

---
+++ UMA_Db.py
@@ -0,0 +1,46 @@
+import psycopg2
+
+class UMA_Db:
+    def __init__(self, hostStr, dbPort, dbStr, uNameStr, dbPassStr) -> None:
+        self.hostStr = hostStr
+        self.dbPort = dbPort
+        self.dbStr = dbStr
+        self.uNameStr = uNameStr
+        self.dbPassStr = dbPassStr
+
+    def insertData(self, uma, tmp1, tmp2, tmp3, tmp4, pre_suministro, pre_retorno, c_motor, prom_efecto_hall, encendido, p_input_valvula, input_valvula, p_output_valvula, output_valvula):
+        try:
+            conn = psycopg2.connect(host=self.hostStr, port=self.dbPort,
+                                    dbname=self.dbStr, user=self.uNameStr, password=self.dbPassStr)
+
+            # get a cursor object from the connection
+            cur = conn.cursor()
+
+            # prepare data insertion rows
+            dataInsertionTuples = [(uma, tmp1, tmp2, tmp3, tmp4, pre_suministro, pre_retorno, c_motor, prom_efecto_hall, encendido, p_input_valvula, input_valvula, p_output_valvula, output_valvula)]
+
+            # create sql command for rows insertion
+            dataText = ','.join(cur.mogrify('(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)', row).decode(
+                "utf-8") for row in dataInsertionTuples)
+
+            sqlTxt = 'INSERT INTO public.uma_data\
+                    (uma, tmp1, tmp2, tmp3, tmp4, pre_suministro, pre_retorno, c_motor, prom_efecto_hall, encendido, p_input_valvula, input_valvula, p_output_valvula, output_valvula)\
+                    VALUES {0} on conflict (uma, data_time) \
+                do update set tmp1 = excluded.tmp1,	tmp2 = excluded.tmp2, tmp3 = excluded.tmp3, tmp4 = excluded.tmp4, pre_suministro = excluded.pre_suministro, pre_retorno = excluded.pre_retorno, c_motor = excluded.c_motor, prom_efecto_hall = excluded.prom_efecto_hall, encendido = excluded.encendido, p_input_valvula = excluded.p_input_valvula, input_valvula = excluded.input_valvula, p_output_valvula = excluded.p_output_valvula, output_valvula = excluded.output_valvula;hum = excluded.hum, tmp=excluded.tmp'.format(dataText)
+
+            # execute the sql to perform insertion
+            cur.execute(sqlTxt)
+
+            rowCount = cur.rowcount
+            print("number of inserted rows =", rowCount)
+
+            # commit the changes
+            conn.commit()
+        except (Exception, psycopg2.Error) as error:
+            print("Error de integración en PostgreSQL...\n", error)
+        finally:
+            if (conn):
+                # close the cursor object to avoid memory leaks
+                cur.close()
+                # close the connection object also
+                conn.close()
---
+++ control.py
@@ -0,0 +1,214 @@
+#Importar librerías
+
+import time
+import board
+import busio
+import glob
+import os
+import datetime
+import pytz
+
+import RPi.GPIO as GPIO
+
+#Módulos ADC
+import Adafruit_MCP4725
+import adafruit_ads1x15.ads1115 as ADS
+from adafruit_ads1x15.analog_in import AnalogIn
+
+
+#Inicializar variables
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
+
+base_dir = '/sys/bus/w1/devices/'
+device_folder = glob.glob(base_dir + '28*')
+
+#Codigo temperatura
+
+def read_temp_raw(device_file):
+    f = open(device_file, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
+
+def read_temp_c(device_file):
+    lines = read_temp_raw(device_file)
+    while True:
+        if lines[0].strip()[-3:] == 'YES':
+            break
+    equals_pos = lines[1].find('t=')
+    if equals_pos != -1:
+        temp_string = lines[1][equals_pos+2:]
+        temp_c = float(temp_string) / 1000.0
+        return temp_c
+
+def get_temperature(device_folder):
+    device_file = device_folder + '/w1_slave'
+    return read_temp_c(device_file)
+
+def sensor_temperatura(device_folder):
+    # Diccionario para guardar listas de temperatura
+    temperature_lists = {}
+
+    for i, device in enumerate(device_folder, start=1):
+        temperature = get_temperature(device)
+        #print(f"Temperature of sensor {i}: {temperature:.2f} C")
+        
+        # Crear una nueva lista para cada sensor si no existe
+        if i not in temperature_lists:
+            temperature_lists[i] = []
+        
+        # Guardar el valor del sensor
+        temperature_lists[i].append(round(temperature,2))
+    return temperature_lists
+
+# Temperatura deseada
+setpoint_temp = 22 # in degrees Celsius
+
+i2c = busio.I2C(board.SCL, board.SDA)
+
+#Modulos ADC
+ads1 = ADS.ADS1115(i2c, address=0x48)
+
+ads1.gain = 1
+V_corriente_motor = AnalogIn(ads1, ADS.P0)
+
+V_p_input = AnalogIn(ads1, ADS.P2)
+V_p_output = AnalogIn(ads1, ADS.P3)
+
+ads2 = ADS.ADS1115(i2c, address=0x49)
+
+ads2.gain = 1
+V_posicion_valvula = AnalogIn(ads2, ADS.P0)
+#V_rpm = AnalogIn(ads2, ADS.P1)
+
+#Feedback válvula
+V_control_valvula = Adafruit_MCP4725.MCP4725(address=0x60, busnum=1)
+
+control_valvula=0
+V_control_valvula.set_voltage(control_valvula)
+
+GPIO.setmode(GPIO.BCM)  # Use BCM pin numbering
+GPIO.setup(25, GPIO.OUT)  # Set GPIO pin 25 as output
+GPIO.output(25, GPIO.LOW)
+
+def efecto_hall():
+    valores = []
+    rpm=0
+    for _ in range():
+        valores.append(V_rpm.voltage)
+        time.sleep(1)   
+    rpm = sum(valores)/len(valores)
+    print(f"Sensor efecto Hall \n Promedio:{rpm}V")
+    if rpm>4:
+        parada_motor()
+        print(f"Error motor, parada de emergencia")    
+
+
+def parada_motor():
+    print("TRAKA")
+    return GPIO.output(25, GPIO.HIGH)
+
+
+def corriente():
+    #Corriente motor
+    V_corriente=round(V_p_input.voltage,2)
+    
+    corriente_motor=round(V_corriente*30.0,2)
+
+    #print(f"Corriente motor \n Voltaje:{V_corriente_motor.voltage}V \n Corriente: {corriente_motor}A")
+    return corriente_motor, V_corriente
+    
+
+def presion():
+    # y=mx+b
+
+    m_p=38.67
+    b_p=0
+    
+    #Suministro
+    V_p_suministro=round(V_p_input.voltage,2)
+    p_suministro=round(m_p*V_p_suministro+b_p,2)
+    #print(f"Presion suministro agua \n Voltaje:{V_p_input.voltage}V \n Presion: {p_input}psi")
+
+
+    V_p_retorno=round(V_p_output.voltage,2)
+    p_retorno=round(m_p*V_p_retorno+b_p,2)
+    #print(f"Presion retorno agua \n Voltaje:{V_p_output.voltage}V \n Presion: {p_output}psi")
+
+    return p_suministro, V_p_suministro, p_retorno, V_p_retorno
+
+
+def f_valvula():
+    #y=mx+b
+    feedback_voltaje=round(V_posicion_valvula.voltage,2)
+    m_val=-29.94
+    b_val=126.24
+    posicion_valvula=round(m_val*feedback_voltaje+b_val,0)
+    
+    return max(0, min(100, posicion_valvula)), feedback_voltaje
+
+def porcentaje_a_bits(porcentaje):
+    # Calcula en numero de bits
+    raw_bits = (porcentaje / 100) * 4096
+    # Redondea al numero entero mas cercano
+    closest_bits = round(raw_bits)
+    # El numero de bits debe estar en el rango valido (0 to 4096)
+    return max(0, min(4096, closest_bits))
+
+def control_valvula(valor_deseado):
+    return V_control_valvula.set_voltage(valor_deseado)
+
+
+
+j=0
+
+
+#Adquisición de datos
+
+try:
+    while True:
+                
+        print('--------------Estado de maquina----------')
+        
+        now = datetime.datetime.now(pytz.timezone('America/Caracas'))
+
+        print("Ciclo", j, now)
+        
+        #Corriente motor
+        corriente_motor, V_corriente=corriente()
+                        
+        #Presion
+        p_suministro, V_p_suministro, p_retorno, V_p_retorno=presion()
+        
+        #Posicion valvula
+        posicion_valvula, feedback_voltaje = f_valvula()
+        
+        #Lista de sensores temperaturas
+        temperature_lists=sensor_temperatura(device_folder)
+
+
+        print("---Corriente en el motor---: \n ", corriente_motor, "A", V_corriente, "V \n --Presión:--- \n Suministro:", p_suministro, "psi", V_p_suministro,"V \n Retorno:",p_retorno,"psi", V_p_retorno,"V \n ---Válvula--- \n Posición:", posicion_valvula, "%", feedback_voltaje, "V \n ---Temperatura:--- \n T1",temperature_lists.get('1'), "°C \n T2",temperature_lists.get('2'), "°C \n T3",temperature_lists.get('3'),"°C \n T4",temperature_lists('4') "°C \n")
+        # print("T1",temperature_lists[1], "T2",temperature_lists[1], "T3",temperature_lists[3],"T4",temperature_lists[4] )
+        
+        
+        """"
+        #Ajuste valvula
+        if valores_temp_salida[j]>setpoint_temp:
+            #x=my+b
+            print(f"Nada")
+        else:
+            #x=my+b
+            control_valvula=-0.0499*posicion_valvula+4.99
+            
+            V_control_valvula.set_voltage(4069)
+            print(f"Posicion deseada valvula \n Voltaje:{control_valvula}V \n Apertura: {posicion_valvula}%")
+                """
+
+        j=1+j
+        time.sleep(10)
+
+except KeyboardInterrupt:
+    pass
+finally:
+    GPIO.cleanup()  # Clean up GP
